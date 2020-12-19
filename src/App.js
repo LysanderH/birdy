5ae0e3b8f3ebf21dc53d8d40bdad6b7b61e65fa6
Lysander Hans <lysander.hans@hotmail.com>
2020-12-19 15:39:04 +0100

    :package: added react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,10 +1,39 @@
+import React, { Fragment } from 'react';
+
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+} from "react-router-dom";
+
+import Home from "./pages/Home";
+
 function App() {
   return (
-    <div className="App">
+    <Fragment>
       <header className="header">
         <h1 className="main-heading"><a href="/" className="main-heading__link" title="Page d'accueil de Birdy">Birdy</a></h1>
+        <Router>
+          <nav className="nav" aria-label="Principale">
+            <h2 className="nav sr-only">Navigation principale</h2>
+            <ul className="nav__list">
+              <li className="nav__item">
+                <Link className="nav__link" to="/">Accueil</Link>
+              </li>
+            </ul>
+          </nav>
+
+          <Switch>
+            <Route path="/">
+              <Home />
+            </Route>
+          </Switch>
+        </Router>
+
+
       </header>
-    </div>
+    </Fragment>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,9 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import firebase from './utils/firebase-config';
-
-console.log(firebase)
 
 ReactDOM.render(
   <React.StrictMode>
